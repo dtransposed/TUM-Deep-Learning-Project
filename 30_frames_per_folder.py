73ae55f0e8e53a268191c04b87e92bd778ea49fa
TenPiotr <34423091+TenPiotr@users.noreply.github.com>
2018-01-16 14:42:50 +0100

    Update 30_frames_per_folder.py

--- 30_frames_per_folder.py
+++ 30_frames_per_folder.py
@@ -6,3 +6,16 @@ Created on Sun Jan 14 14:34:15 2018
 @author: peternagy96
 """
 
+import os
+import shutil
+import pathlib as pth
+  
+dir="C:/Users/tatar/Desktop/gggg"
+directory = os.listdir("C:/Users/tatar/Desktop/gggg")
+for file in directory:
+    print(file)
+    if (int(file) % 2 == 1):
+        hhh= dir + "/" + str(file)
+        shutil.rmtree(hhh)
+    else:
+        continue
